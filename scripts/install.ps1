86751123030d30124d4d044c0688ea7450b959f8
Chris Crutchfield <chris.crutchfield@outlook.com>
2021-12-16 23:21:30 -0800

    install script changes

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -3,39 +3,48 @@ param (
     [string]$URL
 )
 
-# Get all releases
-$releases = ConvertFrom-Json (Invoke-WebRequest "https://api.github.com/repos/clcrutch/dependency-manager/releases").Content
+function Invoke-InstallScript {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$URL
+    )
 
-# Get the latest release
-$maxDate = ($releases.published_at | Measure-Object -Maximum).Maximum
-$latestRelease = $releases | Where-Object { $_.published_at -eq $maxDate }
+    # Get all releases
+    $releases = ConvertFrom-Json (Invoke-WebRequest "https://api.github.com/repos/clcrutch/dependency-manager/releases").Content
 
-# Get the Windows Asset
-$windowsAsset = $latestRelease.assets | Where-Object { $_.name -eq "win7-x64.zip" }
+    # Get the latest release
+    $maxDate = ($releases.published_at | Measure-Object -Maximum).Maximum
+    $latestRelease = $releases | Where-Object { $_.published_at -eq $maxDate }
 
-$tempDirectory = [System.IO.Path]::GetTempPath()
-$tempFolder = "Clcrutch.DependencyManager"
-$tempPath = Join-Path $tempDirectory $tempFolder
+    # Get the Windows Asset
+    $windowsAsset = $latestRelease.assets | Where-Object { $_.name -eq "win7-x64.zip" }
 
-$windowsAssetFolder = [System.IO.Path]::GetFileNameWithoutExtension($windowsAsset.name)
+    $tempDirectory = [System.IO.Path]::GetTempPath()
+    $tempFolder = "Clcrutch.DependencyManager"
+    $tempPath = Join-Path $tempDirectory $tempFolder
 
-if (Test-Path $tempPath) {
-    Remove-Item $tempPath -Force -Recurse | Out-Null
+    $windowsAssetFolder = [System.IO.Path]::GetFileNameWithoutExtension($windowsAsset.name)
+
+    if (Test-Path $tempPath) {
+        Remove-Item $tempPath -Force -Recurse | Out-Null
+    }
+    New-Item -Path $tempDirectory -Name $tempFolder -ItemType Directory | Out-Null
+
+    Push-Location $tempPath
+
+    # Get the executable
+    Invoke-WebRequest $windowsAsset.browser_download_url -OutFile $windowsAsset.name | Out-Null
+    Expand-Archive -Path $windowsAsset.name -DestinationPath $windowsAssetFolder
+
+    Push-Location $windowsAssetFolder
+    $executable = [System.IO.Path]::Combine($tempPath, $windowsAssetFolder, "DependencyManager.exe")
+
+    Invoke-WebRequest $URL -OutFile "dependencies.yaml"
+
+    & $executable install
+
+    Pop-Location
+    Pop-Location
 }
-New-Item -Path $tempDirectory -Name $tempFolder -ItemType Directory | Out-Null
 
-Push-Location $tempPath
-
-# Get the executable
-Invoke-WebRequest $windowsAsset.browser_download_url -OutFile $windowsAsset.name | Out-Null
-Expand-Archive -Path $windowsAsset.name -DestinationPath $windowsAssetFolder
-
-Push-Location $windowsAssetFolder
-$executable = [System.IO.Path]::Combine($tempPath, $windowsAssetFolder, "DependencyManager.exe")
-
-Invoke-WebRequest $URL -OutFile "dependencies.yaml"
-
-& $executable install
-
-Pop-Location
-Pop-Location
+Invoke-InstallScript -URL $URL
